a5f15e4fd8419e9a15c485248dc31361008b654b
schultkr <schult.christoph@googlemail.com>
2016-02-06 13:46:02 +0100

    Chapter 7
    Qs in chapter 7

---
+++ BCS_ArchimedeanContour/BCS_ArchimedeanContour.R
@@ -0,0 +1,60 @@
+library(copula)
+
+graphics.off()
+layout(matrix(c(1,2,3,4,5,6), 3, 2, byrow = TRUE)) # defines number of plots and their respective positions
+    par(pty="s",mar=c(2.1,2.5,2.1,2.5))            # graphical parameters see ?par()
+
+# Gumbel Copulae with theta=2
+
+# Gumbel Copula for normal marginal distributions
+gumMVD<-mvdc(gumbelCopula(2),                                            
+             margins=c("norm","norm"),                                   
+             paramMargins=list(list(mean=0,sd=1.2),list(mean=0,sd=1.2))) # marginal distribution parameters
+
+# Contour Plot: Gumbel Copula based on normal marginal distributions 
+contour(gumMVD, dMvdc, xlim = c(-2.5, 2.5), ylim = c(-2.5, 2.5),         
+        cex.axis = 1.5, labcex=1.5, lwd = 3)                             # random variables
+
+
+#Gumbel Copula for t6 marginal distributions
+gumMVD<-mvdc(gumbelCopula(2),margins=c("t","t"),                         
+             paramMargins=list(list(df=6),list(df=6)))                   # marginal distribution parameter
+
+# Contour Plot: Gumbel Copula based on t6 marginal distributions
+contour(gumMVD, dMvdc, xlim = c(-2.5, 2.5), ylim = c(-2.5, 2.5),        
+        cex.axis = 1.5, labcex=1.5, lwd = 3)                            
+
+# Clayton Copulae theta=2
+
+# Clayton Copula for normal marginal distributions
+clayMVD<-mvdc(claytonCopula(2),margins=c("norm","norm"),
+              paramMargins=list(list(mean=0,sd=1.2),list(mean=0,sd=1.2)))
+
+# Contour Plot: Clayton Copula for normal marginal distributions
+contour(clayMVD, dMvdc, xlim = c(-2.5, 2.5), ylim = c(-2.5, 2.5),
+        cex.axis = 1.5, labcex=1.5, lwd = 3)  
+
+# Clayton Copula for t6 marginal distributions
+clayMVD<-mvdc(claytonCopula(2),margins=c("t","t"),paramMargins=list(list(df=6),list(df=6)))
+
+# Contour Plot: Clayton Copula for t6 marginal distributions
+contour(clayMVD, dMvdc, xlim = c(-2.5, 2.5), ylim = c(-2.5, 2.5),
+        cex.axis = 1.5, labcex=1.5, lwd = 3)
+
+# Frank Copulae theta=2
+
+# Frank Copula for normal marginal distributions
+frankMVD<-mvdc(frankCopula(2),margins=c("norm","norm"),
+               paramMargins=list(list(mean=0,sd=1.2),list(mean=0,sd=1.2)))
+
+# Contour Plot: Frank Copula for normal marginal distributions
+contour(frankMVD, dMvdc, xlim = c(-2.5, 2.5), ylim = c(-2.5, 2.5),
+        nlevels=10, cex.axis = 1.5, labcex=1.5, lwd = 3)
+
+# Frank Copula for t6 marginal distributions
+frankMVD<-mvdc(frankCopula(2),margins=c("t","t"),
+               paramMargins=list(list(df=6),list(df=6)))
+
+# Contour Plot: Frank Copula for t6 marginal distributions
+contour(frankMVD, dMvdc, xlim = c(-2.5, 2.5), ylim = c(-2.5, 2.5),
+        nlevels=10, cex.axis = 1.5, labcex=1.5, lwd = 3)
---
+++ BCS_NormalProbability/BCS_NormalProbability.R
@@ -0,0 +1,19 @@
+# make sure the package is installed
+#install.packages("fMultivar")
+require(fMultivar)
+
+# defines how the plots will appear 
+layout(matrix(c(1,2), 1, 2))
+# vector of random variable
+x = (-45:45)/20
+# produces X with columns x and y
+X = grid2d(x)
+# bivariate normal distribution for x and y
+z = pnorm2d(X$x, X$y, rho = 0.7)
+# produces matrix Z with three columns (x,y,z)
+# z is the pdf of y and x
+Z = list(x = x, y = x, z = matrix(z, ncol = length(x)))
+# 3D scatterplot of bivariate normal pdf
+persp(Z, theta = -15, phi = 25, col = "steelblue")
+# contour plot of Z
+contour(Z, xlim = c(-2.5,2.5), ylim = c(-2.5,2.5), lwd = 4, col = heat.colors(80), nlevels = 10, cex.axis=1.5, labcex=1.7)
