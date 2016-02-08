38347318aa260b42f93e7e14a656541d63769879
anna <wagneran7@gmail.com>
2016-02-08 21:46:12 +0100

    Update BCS_RANDU.r
    "<-" is changed through "="

--- BCS_RANDU/BCS_RANDU.R
+++ BCS_RANDU/BCS_RANDU.R
@@ -1,9 +1,9 @@
 # set a seed here or inside the RANDU function seed = 1
 
 # Implementation of the RANDU generator (with seed=1)
-RANDU <- function(n, seed = 1) {
+RANDU = function(n, seed = 1) {
     # predefine constants
-    U <- NULL
+    U = NULL
     a = 2^16 + 3
     m = 2^31
     for (i in 1:n) {
