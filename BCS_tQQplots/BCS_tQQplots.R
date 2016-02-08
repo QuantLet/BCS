870262610950c2cd50c4183d33b2c2479abf2d2e
anna <wagneran7@gmail.com>
2016-02-08 21:50:56 +0100

    Update BCS_tQQplots.R

--- BCS_tQQplots/BCS_tQQplots.R
+++ BCS_tQQplots/BCS_tQQplots.R
@@ -1,14 +1,14 @@
 par(mfrow = c(1, 2))
 
 # create random sample which is t-distributed
-x <- rt(100, df = 3)
+x = rt(100, df = 3)
 # quantiles of t with 3 degrees of freedom
 qqnorm(x, col = "red", pch = 20, asp = 1)
 # line of perfect fit
 qqline(x)
 
 # create random sample which is t-distributed
-y <- rt(100, df = 100)
+y = rt(100, df = 100)
 # quantiles of t with 100 degrees of freedom
 qqnorm(y, col = "red", pch = 20, asp = 1)
 # line of perfect fit
