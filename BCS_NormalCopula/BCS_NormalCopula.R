d71e64929fb55ad9bf0755db86e56b0d8f054dfa
anna <wagneran7@gmail.com>
2016-02-08 21:45:03 +0100

    Update BCS_NormalCopula.R
    "<-" is changed through "="

--- BCS_NormalCopula/BCS_NormalCopula.R
+++ BCS_NormalCopula/BCS_NormalCopula.R
@@ -4,7 +4,7 @@ layout(matrix(c(1, 1, 2, 3, 4, 5), 3, 2, byrow = T), TRUE)
 par(pty = "s")
 
 # bivariate normal copula with correlation 0.7 for the 2 variables
-norm.2d.cop <- normalCopula(param = 0.7, dim = 2)
+norm.2d.cop = normalCopula(param = 0.7, dim = 2)
 
 # scatterplot of a pseudo random sample of two variables with a joint normal distribution
 plot(rCopula(1000, norm.2d.cop), pch = 19, cex.axis = 1.5, cex.lab = 1.5, xlab = "x", ylab = "y")
