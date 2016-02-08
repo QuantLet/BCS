07fcb03f4bef1b2d0c06b60fad147194ffe5c169
anna <wagneran7@gmail.com>
2016-02-08 21:40:50 +0100

    Update R-file
     "<-" is changed through "="

--- BCS_claytonMC/BCS_claytonMC.r
+++ BCS_claytonMC/BCS_claytonMC.r
@@ -11,15 +11,15 @@ library(lattice)
 theta = 1  #Set theta [0,1]
 
 # variables uniformly distributed:
-uniclayMVD <- mvdc(claytonCopula(theta), margins = c("unif", "unif"), paramMargins = list(list(min = 0, max = 1), list(min = 0, 
+uniclayMVD = mvdc(claytonCopula(theta), margins = c("unif", "unif"), paramMargins = list(list(min = 0, max = 1), list(min = 0, 
     max = 1)))
-uniclay <- rMvdc(uniclayMVD, n = 10000)
+uniclay = rMvdc(uniclayMVD, n = 10000)
 
 plot(uniclay, xlab = "", ylab = "", pch = 19, main = "Uniform")
 
 
 # variables standard normally distributed:
-normclayMVD <- mvdc(claytonCopula(theta), margins = c("norm", "norm"), paramMargins = list(list(mean = 0, sd = 1), list(mean = 0, 
+normclayMVD = mvdc(claytonCopula(theta), margins = c("norm", "norm"), paramMargins = list(list(mean = 0, sd = 1), list(mean = 0, 
     sd = 1)))
 normclay <- rMvdc(normclayMVD, n = 10000)
 
