0580ebdeb6c871a34218ece1510f305162cf3b36
anna <wagneran7@gmail.com>
2016-03-14 12:25:12 +0100

    Update

--- BCS_Binpois/BCS_Binpois.R
+++ BCS_Binpois/BCS_Binpois.R
@@ -2,8 +2,8 @@
 par(cex.lab = 1.2, cex.main = 1.1, mar = c(4, 4.5, 3, 2) + 0.1, mgp = c(2.9, 1, 0))
 
 # set the values
-n = 20
-x = 0:n
+     n = 20
+     x = 0:n
 lambda = 10
 
 # Create a poisson distribution with x and lambda and plot it
@@ -11,7 +11,7 @@ prob = dpois(x, lambda)
 plot(x, prob, type = "h", main = "Pois(10) vs. B(100, 0.1)", ylab = "Probability", ylim = c(0, 0.15))
 
 # Create a binomial distribution and add it as dots.
-s = 0:100
-f = dbinom(x = s, 100, 0.1)
+    s = 0:100
+    f = dbinom(x = s, 100, 0.1)
 
 points(0:100, f, pch = 16, cex = 1.5, col = "black")
